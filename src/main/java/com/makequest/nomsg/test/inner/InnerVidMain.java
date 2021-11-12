61f5cf0e6090c0259fb4b7c4b13fed30e7abf8a4
Junhee <bag777@ipageon.com>
2021-11-12 21:31:06 +0900

    Inner messaging test applcation - Vid test

--- src/main/java/com/makequest/nomsg/test/inner/InnerVidMain.java
+++ src/main/java/com/makequest/nomsg/test/inner/InnerVidMain.java
@@ -2,10 +2,10 @@ package com.makequest.nomsg.test.inner;
 
 public class InnerVidMain {
     public static void main(String[] args) throws InterruptedException {
-        VidPeer peer1 = new VidPeer("Peer1", "TestVid1", "TestVid2", 3);
-        VidPeer peer2 = new VidPeer("Peer2", "TestVid1", "TestVid2", 4);
-        VidPeer peer3 = new VidPeer("Peer3", "TestVid1", "TestVid2", 5);
-        VidPeer peer4 = new VidPeer("Peer4", "TestVid2", "TestVid1", 6);
+        VidPeer peer1 = new VidPeer("Peer1", null, "TestVid", 3);
+        VidPeer peer2 = new VidPeer("Peer2", "TestVid", null, 4);
+        VidPeer peer3 = new VidPeer("Peer3", "TestVid", null, 5);
+        VidPeer peer4 = new VidPeer("Peer4", "TestVid", null, 6);
 
         new Thread(peer1).start();
         new Thread(peer2).start();

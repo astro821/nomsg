88965459cd5f28e0cdad2f0d9ec5c74e6348c260
Junhee <bag777@ipageon.com>
2021-11-13 04:18:18 +0900

    TCP Server 개발

--- src/main/java/com/makequest/nomsg/router/MeshConnectionHandleImpl.java
+++ src/main/java/com/makequest/nomsg/router/MeshConnectionHandleImpl.java
@@ -72,7 +72,7 @@ public class MeshConnectionHandleImpl implements MeshConnectionHandle {
         } else {
             throw new NoMsgNetworkException("server already initialized.");
         }
-        TcpClientConnManager.getInstance();
+        TcpClientConnManager.getInstance().setSourcePort(port);
     }
 
     @Override

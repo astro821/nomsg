3860a6fdc0c7385bb992d66e32354c04eff1251f
Dong-uk, Kim <eastiris@gmail.com>
2021-11-15 14:44:19 +0900

    #5 Closed

--- src/main/java/com/makequest/nomsg/exception/NoMsgRouterException.java
+++ src/main/java/com/makequest/nomsg/exception/NoMsgRouterException.java
@@ -1,4 +1,10 @@
 package com.makequest.nomsg.exception;
 
 public class NoMsgRouterException extends Exception{
+    public NoMsgRouterException(String message) {
+        super(message);
+    }
+
+    public NoMsgRouterException() {
+    }
 }
